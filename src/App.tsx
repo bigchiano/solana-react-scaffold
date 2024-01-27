0eabbe277d37cf08aadb60bcdc034b5b4c72ca01
bigchiano <bigchiano@protonmail.com>
2024-01-27 02:42:36 +0200

    remove unsed imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import MyWallet from "./components/my-wallet";
 import TransferSol from "./pages/TransferForm";
